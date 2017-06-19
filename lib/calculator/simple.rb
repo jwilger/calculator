9c3d3c93e98f078254822e70962f4486a6fad1c7
John Wilger <john@johnwilger.com>
2017-06-18 21:38:41 -0700

    Implements calculator logic

--- lib/calculator/simple.rb
+++ lib/calculator/simple.rb
@@ -13,14 +13,43 @@ module Calculator
     end
 
     def call
-      failure.call('Invalid symbols in operation. ' \
-                   'Only integers and basic math ' \
-                   'operators (+, -, *, and /) are ' \
-                   'supported.')
+      return unless valid_operation?
+      self.result = operation.dup
+      perform_multiplication_and_division
+      perform_addition_and_subtraction
+      success.call(result.to_f)
     end
 
     private
 
-    attr_accessor :operation, :success, :failure
+    attr_accessor :operation, :success, :failure, :result
+
+    def valid_operation?
+      return true if /^[\d+\-*\/]*$/ =~ operation
+      failure.call('Invalid symbols in operation. ' \
+                   'Only integers and basic math ' \
+                   'operators (+, -, *, and /) are ' \
+                   'supported.')
+      false
+    end
+
+    def perform_multiplication_and_division
+      perform_operation_set('*/')
+    end
+
+    def perform_addition_and_subtraction
+      perform_operation_set('+-')
+    end
+
+    def perform_operation_set(operation_set)
+      while %r{[#{operation_set}]} =~ result
+        result.sub!(%r{(\d+(\.\d+)?)([#{operation_set}])(\d+(\.\d+)?)}) do
+          lhs = $1.to_f
+          operator = $3
+          rhs = $4.to_f
+          lhs.send(operator, rhs)
+        end
+      end
+    end
   end
 end
--- spec/calculator/simple_spec.rb
+++ spec/calculator/simple_spec.rb
@@ -17,22 +17,50 @@ RSpec.describe Calculator::Simple do
     instance_double('Proc', 'failure', call: nil)
   }
 
+  before(:each) do
+    subject.call
+  end
+
   context 'when the provided operation contains characters other than ' \
           'integers or basic math operations' do
     let(:operation) { '2*(3+4)' }
 
     it 'does not call the success handler' do
-      subject.call
       expect(success_handler).not_to have_received(:call)
     end
 
     it 'calls the failure handler with an error message' do
-      subject.call
       expect(failure_handler)
         .to have_received(:call).with('Invalid symbols in operation. ' \
                                       'Only integers and basic math ' \
                                       'operators (+, -, *, and /) are ' \
                                       'supported.')
+    end
+  end
+
+  context 'operation: 5*3+1+6/2+9*100' do
+    let(:operation) { '5*3+1+6/2+9*100' }
+    let(:expected_result) { 5.0*3.0+1.0+6.0/2.0+9.0*100.0 }
+
+    it 'calls the success handler with the correct result, 919' do
+      expect(success_handler).to have_received(:call).with(expected_result)
+    end
+
+    it 'does not call the failure handler' do
+      expect(failure_handler).not_to have_received(:call)
+    end
+  end
+
+  context 'operation: 5*3+1+6/85+9*100' do
+    let(:operation) { '5*3+1+6/85+9*100' }
+    let(:expected_result) { 5.0*3.0+1.0+6.0/85.0+9.0*100.0 }
+
+    it 'calls the success handler with the correct result, 916.07' do
+      expect(success_handler).to have_received(:call).with(expected_result)
+    end
+
+    it 'does not call the failure handler' do
+      expect(failure_handler).not_to have_received(:call)
     end
   end
 end
